a4ea620d06e8608c6562f14d9eddd2f61448449c
tapadipti <tapadipti@gmail.com>
2022-12-23 20:34:16 +0545

    Fix accuracy metric

--- test_live_metrics.py
+++ test_live_metrics.py
@@ -8,13 +8,13 @@ params = dvc.api.params_show()
 
 with Live() as live:
     # live.log_param("trigger", random.random())
-    accuracy = 0.82
+    accuracy = 0.84
     for i in range(params["epochs"]):
         # live.log_metric("foo", i + random.random())
         # live.log_metric("bar", i + random.random())
         live.log_metric("accuracy", accuracy)
 
-        accuracy += random.uniform(0.001, 0.02)
+        accuracy += random.uniform(0.007, 0.03)
         if accuracy > 0.98:
             accuracy = random.uniform(0.98, 0.99)
         
