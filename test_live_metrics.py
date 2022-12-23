de9e6487f3233f349d2e8986756ceb66a819a3cc
tapadipti <tapadipti@gmail.com>
2022-12-23 17:03:15 +0545

    Fix accuracy metric

--- test_live_metrics.py
+++ test_live_metrics.py
@@ -12,10 +12,12 @@ with Live() as live:
     for i in range(params["epochs"]):
         # live.log_metric("foo", i + random.random())
         # live.log_metric("bar", i + random.random())
+        live.log_metric("accuracy", accuracy)
+
         accuracy += random.uniform(0.001, 0.02)
         if accuracy > 0.98:
             accuracy = random.uniform(0.98, 0.99)
-        live.log_metric("accuracy", accuracy)
+        
         live.next_step()
 
         time.sleep(6)
